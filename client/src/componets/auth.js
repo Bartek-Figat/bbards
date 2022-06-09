a18292371ff8d03cd4a2f76a775ba1818b15e3e5
Bartek-Figat <figat29@gmail.com>
2022-06-08 22:20:44 -0400

    cors

--- client/src/componets/auth.js
+++ client/src/componets/auth.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { authenticationPage } from '../slice/authenticationSlice';
 
 export const Authentication = () => {
