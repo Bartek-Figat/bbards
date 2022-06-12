654d5d2a7e8206d61fc1befd256047a9425f2606
Bartek-Figat <figat29@gmail.com>
2022-06-11 21:21:21 -0400

    try logout

--- client/src/componets/logout.js
+++ client/src/componets/logout.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function Logout() {
   const jwt = localStorage.getItem('token');
-  await axios.delete('http://localhost:8080/logout', {
+  await axios.delete('http://api.bbards.com/api/v1/logout', {
     credentials: 'include',
     headers: {
       Authorization: jwt,
